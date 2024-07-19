d2fbcec239a3317b9a96043f51533d6137246764
Mahziyar Esmaili <mahziyar.esmaili.dev@gmail.com>
2024-07-19 08:23:34 +0200

    added basic terraform file containing s3/sqs/dynamodb resources

---
+++ terraform/main.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+
+  access_key = "test"
+  secret_key = "test"
+  region     = "us-east-1"
+
+
+  s3_use_path_style           = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    s3       = "http://s3.localhost.localstack.cloud:4566"
+    sqs      = "http://localhost:4566"
+    dynamodb = "http://localhost:4566"
+  }
+}
+
+
+resource "aws_s3_bucket" "raw_videos_bucket" {
+  bucket = "raw-videos-bucket"
+}
+
+resource "aws_s3_bucket" "processes_videos_bucket" {
+  bucket = "processed-videos-bucket"
+}
+
+resource "aws_sqs_queue" "queue" {
+  name = "queue"
+}
+
+resource "aws_dynamodb_table" "videos" {
+  name         = "videos"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "key"
+
+  attribute {
+    name = "key"
+    type = "S"
+  }
+}
