64a28347a7f2759611a865febb7dd77ba73f8b8b
Mahziyar Esmaili <mahziyar.esmaili.dev@gmail.com>
2024-07-28 07:40:17 +0200

    created a custom multi file validator pipe

---
+++ apps/backend/src/videos/multi-file-validator.pipe.ts
@@ -0,0 +1,29 @@
+import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
+
+@Injectable()
+export class MultiFileValidatorPipe implements PipeTransform {
+  constructor(
+    private readonly validations: Record<
+      string,
+      { required?: boolean; mimetype?: string[] }
+    >,
+  ) {}
+
+  transform(value: any) {
+    for (const [key, validationOptions] of Object.entries(this.validations)) {
+      const file = value?.[key]?.[0];
+
+      if (validationOptions.required && !file) {
+        throw new BadRequestException(`${key} is required`);
+      }
+      if (
+        validationOptions.mimetype.length > 0 &&
+        !validationOptions.mimetype.includes(file.mimetype)
+      ) {
+        throw new BadRequestException(
+          `${key} must be of type ${validationOptions.mimetype}`,
+        );
+      }
+    }
+  }
+}
