9a096e92a97b9d3424f865e3ccc3acf25444e362
Mahziyar Esmaili <mahziyar.esmaili.dev@gmail.com>
2024-07-30 19:50:21 +0200

    modified httpRequest function in frontend to read api base url from env variables

--- apps/frontend/src/utils.ts
+++ apps/frontend/src/utils.ts
@@ -1,5 +1,5 @@
 export const httpRequest = async (url: string, options?: RequestInit) => {
-  const res = await fetch(`http://localhost:3001/${url}`, options);
+  const res = await fetch(`${process.env.API_BASE_URL}/${url}`, options);
 
   const data = await res.json();
 
